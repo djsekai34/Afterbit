e137a8be94bd0f24a8a56bd65268404fef77ff1b
David Jiménez Villena <davidjimenezvillena@gmail.com>
2025-10-29 10:30:43 +0100

    Afterbit V 0.0.1.3

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import viteLogo from '/Afterbit-01.svg'
 import './App.css'
 import Barra from "./components/barra.jsx";
 import "./index.css";
